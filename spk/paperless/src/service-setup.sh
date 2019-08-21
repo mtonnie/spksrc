9553583e85d70926f9bfff4ed02f289416c7c97a
Tonnier <mtonnie@emea.corpdir.net>
2019-08-21 08:58:24 +0200

    Update service-setup.sh

--- spk/paperless/src/service-setup.sh
+++ spk/paperless/src/service-setup.sh
@@ -2,13 +2,12 @@ PYTHON_DIR="/usr/local/python3"
 VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
 PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:${PYTHON_DIR}/bin:${PATH}"
 
+CFG_FILE="${SYNOPKG_PKGDEST}/etc/paperless"
 MANAGE_PY="${SYNOPKG_PKGDEST}/share/paperless/manage.py"
 
 PAPERLESS_DBDIR="${SYNOPKG_PKGDEST}/var/database"
 PAPERLESS_STATICDIR="${SYNOPKG_PKGDEST}/var/static"
 PAPERLESS_MEDIADIR="${SYNOPKG_PKGDEST}/var/media"
-
-PAPERLESS_ENV="PAPERLESS_DBDIR=\"${PAPERLESS_DBDIR}\" PAPERLESS_STATICDIR=\"${PAPERLESS_STATICDIR}\" PAPERLESS_MEDIADIR=\"${PAPERLESS_MEDIADIR}\""
 
 service_postinst ()
 {
@@ -18,33 +17,33 @@ service_postinst ()
     # Install the wheels
     ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --no-index -U --force-reinstall -f ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl >> ${INST_LOG} 2>&1
 
-    # Add symlink
+    #Create configuration file /etc/paperless.conf
+    echo "PAPERLESS_DBDIR=$PAPERLESS_DBDIR" >> ${CFG_FILE}
+    echo "PAPERLESS_STATICDIR=$PAPERLESS_STATICDIR" >> ${CFG_FILE}
+    echo "PAPERLESS_MEDIADIR=$PAPERLESS_MEDIADIR" >> ${CFG_FILE}
+    #if [[ "$wizard_provider_gdriv" == "true" ]]; then
+    #        echo "TRANSFERSH_PROVIDER=gdrive" >> ${CFG_FILE}
+    #fi
+
+    #Create directories
     if [ ! -d ${PAPERLESS_STATICDIR} ]; then
         mkdir -p ${PAPERLESS_STATICDIR}
     fi
-    ${PAPERLESS_ENV} ${SYNOPKG_PKGDEST}/env/bin/python3 ${MANAGE_PY} collectstatic >> ${INST_LOG}
-
     if [ ! -d ${PAPERLESS_DBDIR} ]; then
         mkdir -p ${PAPERLESS_DBDIR}
     fi
-    ${SYNOPKG_PKGDEST}/env/bin/python3 ${MANAGE_PY} makemigrations >> ${INST_LOG}
-    ${PAPERLESS_ENV} ${SYNOPKG_PKGDEST}/env/bin/python3 ${MANAGE_PY} migrate >> ${INST_LOG}
-    
-    ${PAPERLESS_ENV} ${SYNOPKG_PKGDEST}/env/bin/python3 ${MANAGE_PY} shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')"
-
     if [ ! -d ${PAPERLESS_MEDIADIR} ]; then
         mkdir -p ${PAPERLESS_MEDIADIR}
     fi
-
     
+    #Collect static files
+    ${SYNOPKG_PKGDEST}/env/bin/python3 ${MANAGE_PY} collectstatic >> ${INST_LOG}
+    
+    #Initialize database
+    ${SYNOPKG_PKGDEST}/env/bin/python3 ${MANAGE_PY} makemigrations >> ${INST_LOG}
+    ${SYNOPKG_PKGDEST}/env/bin/python3 ${MANAGE_PY} migrate >> ${INST_LOG}
+    
+    #Create admin user
+    ${SYNOPKG_PKGDEST}/env/bin/python3 ${MANAGE_PY} shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')"
 
-    #mkdir -p /usr/local/bin
-    #ln -s ${SYNOPKG_PKGDEST}/env/bin/borg /usr/local/bin/borg
-    #ln -s ${SYNOPKG_PKGDEST}/env/bin/borgmatic /usr/local/bin/borgmatic
 }
-
-#service_postuninst ()
-##{
-    #rm -f /usr/local/bin/borg
-    #rm -f /usr/local/bin/borgmatic
-#}
