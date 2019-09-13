f4c792d099a0ea836bcfa0972431db8041599aad
Martin Tonnier <martin@tonnier.name>
2019-09-13 15:51:51 +0000

    .

--- spk/paperless/src/service-setup.sh
+++ spk/paperless/src/service-setup.sh
@@ -2,12 +2,18 @@ PYTHON_DIR="/usr/local/python3"
 VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
 PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:${PYTHON_DIR}/bin:${PATH}"
 
-CFG_FILE="${SYNOPKG_PKGDEST}/etc/paperless"
+CFG_FILE="${SYNOPKG_PKGDEST}/var/paperless.conf"
 MANAGE_PY="${SYNOPKG_PKGDEST}/share/paperless/manage.py"
 
 PAPERLESS_DBDIR="${SYNOPKG_PKGDEST}/var/database"
 PAPERLESS_STATICDIR="${SYNOPKG_PKGDEST}/var/static"
 PAPERLESS_MEDIADIR="${SYNOPKG_PKGDEST}/var/media"
+
+PAPERLESS_ENV="PAPERLESS_DBDIR=${PAPERLESS_DBDIR} PAPERLESS_STATICDIR=${PAPERLESS_STATICDIR} PAPERLESS_MEDIADIR=${PAPERLESS_MEDIADIR}"
+
+SERVICE_COMMAND="${PAPERLESS_ENV} ${SYNPKG_PKGDEST}/env/python3 ${MANAGE_PY} runserver"
+
+GROUP="sc-paperless"
 
 service_postinst ()
 {
@@ -37,13 +43,13 @@ service_postinst ()
     fi
     
     #Collect static files
-    ${SYNOPKG_PKGDEST}/env/bin/python3 ${MANAGE_PY} collectstatic >> ${INST_LOG}
+    ${PAPERLESS_ENV} ${SYNOPKG_PKGDEST}/env/bin/python3 ${MANAGE_PY} collectstatic >> ${INST_LOG}
     
     #Initialize database
-    ${SYNOPKG_PKGDEST}/env/bin/python3 ${MANAGE_PY} makemigrations >> ${INST_LOG}
-    ${SYNOPKG_PKGDEST}/env/bin/python3 ${MANAGE_PY} migrate >> ${INST_LOG}
+    ${PAPERLESS_ENV} ${SYNOPKG_PKGDEST}/env/bin/python3 ${MANAGE_PY} makemigrations >> ${INST_LOG}
+    ${PAPERLESS_ENV} ${SYNOPKG_PKGDEST}/env/bin/python3 ${MANAGE_PY} migrate >> ${INST_LOG}
     
     #Create admin user
-    ${SYNOPKG_PKGDEST}/env/bin/python3 ${MANAGE_PY} shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')"
+    ${PAPERLESS_ENV} ${SYNOPKG_PKGDEST}/env/bin/python3 ${MANAGE_PY} shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')"
 
 }
