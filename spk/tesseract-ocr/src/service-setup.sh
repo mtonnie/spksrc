392a0c2c233cdfb7ea3469160c46e1d10f53208f
mtonnie <martin@tonnier.name>
2020-08-14 15:19:04 +0000

    fixed installation procedure for tessdata

--- spk/tesseract-ocr/src/service-setup.sh
+++ spk/tesseract-ocr/src/service-setup.sh
@@ -8,19 +8,20 @@ service_postinst () {
     echo "WIZARD_KIND=${wizard_kind}" >> ${INST_VARIABLES}
     echo "WIZARD_VERSION=${wizard_version}" >> ${INST_VARIABLES}
     LANGS=$(ls "${SYNOPKG_PKGDEST}/share/tessdata/" | grep ".*traineddata$" | sed -n -e 's/.traineddata$//p')
+    echo "$LANGS" >> ${INST_lOG}
     for LANG in $LANGS
     do
         echo "$LANG" >> ${INST_lOG}
         rm -f "${SYNOPKG_PKGDEST}/share/tessdata/${LANG}"
         var="wizard_lang_${LANG}"
         if [ "${!var}" == "true" ]; then
-          curl "https://github.com/tesseract-ocr/$wizard_kind/raw/$wizard_version/${LANG}.traineddata" -o "${SYNOPKG_PKGDEST}/share/tessdata/${LANG}.traineddata"
+          curl -L "https://github.com/tesseract-ocr/$wizard_kind/raw/$wizard_version/${LANG}.traineddata" -o "${SYNOPKG_PKGDEST}/share/tessdata/${LANG}.traineddata"
         fi
     done
     printenv | grep "^wizard_lang_.*=true$" | sed -n -e 's/=.*$//p' | while read -r line ; do
         if [ "${!line}" == "true" ]; then
             URL="https://github.com/tesseract-ocr/$wizard_kind/raw/$wizard_version/${line:12}.traineddata"
-            curl "$URL" -o "${SYNOPKG_PKGDEST}/share/tessdata/${line:12}.traineddata" >> ${INST_LOG}
+            curl -L "$URL" -o "${SYNOPKG_PKGDEST}/share/tessdata/${line:12}.traineddata" >> ${INST_LOG}
         fi
     done
     mkdir -p /usr/local/bin
